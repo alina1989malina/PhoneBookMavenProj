31a5965a7d1d65904894c77b3672d62dfb981fc0
alina <alina1989malina@yandex.ru>
2019-02-17 16:30:08 +0300

    fix encoding trouble + add gitignore

--- src/main/java/pack/pb/PbApp.java
+++ src/main/java/pack/pb/PbApp.java
@@ -3,18 +3,18 @@ package pack.pb;
 import pack.pb.entity.PhoneBook;
 import pack.pb.entity.implementation.PhoneBookImpl;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class PbApp {
 
     public static void main( String[] args ) {
-        System.out.println( System.getProperty("file.encoding"));
         PhoneBook pb = PhoneBookImpl.getInstance();
 
-        Scanner scanner = new Scanner(System.in, "UTF-8");
-        System.out.println("Введите ФИО: ");
-        String fullName = scanner.nextLine();
-
+        Console c = System.console();
+        System.out.println("Введите ФИО:");
+        String fullName = c != null ? c.readLine("") :
+                new Scanner(System.in, "UTF-8").nextLine();
 
         try{
             pb.printPhoneBookItem(fullName);
